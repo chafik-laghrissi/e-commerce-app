54982cbc9163ad1465e0d6ea56a392dbb0d16884
Chafik LAGHRISSI <laghrissichafik@gmail.com>
2020-10-05 22:52:48 +0100

    add billing and client side app

--- app.js
+++ app.js
@@ -36,6 +36,14 @@ require("./routes/itemRoutes")(app);
 require("./routes/sectionRoutes")(app);
 require("./routes/cartRoutes")(app);
 require("./routes/billingRoutes")(app);
+if(process.env.NODE_ENV==="production")
+{
+  app.use(express.static(__dirname+"/client/build"));
+  const path=require("path");
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+  })
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("your server is running on port " + port);
