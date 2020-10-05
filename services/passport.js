db680d17b33fe1fff4a7de5acebb4c4e6fe7485f
Chafik LAGHRISSI <laghrissichafik@gmail.com>
2020-10-05 23:31:38 +0100

    debug for 3 time

--- services/passport.js
+++ services/passport.js
@@ -47,7 +47,7 @@ passport.use(
 passport.use(
   new FacebookStrategy(
     {
-      clientID: process.env.FACEBOOK_APP_ID,
+    clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
     callbackURL: "/auth/facebook/callback"
     },
