cfc7d87e26280913773cf8ec4ef46d6d3384c539
Chafik LAGHRISSI <laghrissichafik@gmail.com>
2020-10-05 23:07:27 +0100

    make it better

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ require("./routes/cartRoutes")(app);
 require("./routes/billingRoutes")(app);
 if(process.env.NODE_ENV==="production")
 {
-  app.use(express.static(__dirname+"/client/build"));
+  app.use(express.static("/client/build"));
   const path=require("path");
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"client","build","index.html"));
