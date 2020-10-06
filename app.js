ebc4397f59e80a85257e7986eca006eec0bbace6
Chafik LAGHRISSI <laghrissichafik@gmail.com>
2020-10-06 01:52:21 +0100

    hello again

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ require("./routes/billingRoutes")(app);
 if(process.env.NODE_ENV==="production")
 {
   app.use(express.static("/client/build"));
-  // const path=require("path");
-  app.get("/*",(req,res)=>{
-    res.sendFile("./client/build/index.html");
+  const path=require("path");
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"));
   })
 }
 const port = process.env.PORT || 5000;
