99cab897f59ec8f11b0d379d8c4533b8756fae93
Chafik LAGHRISSI <laghrissichafik@gmail.com>
2020-10-06 01:38:39 +0100

    hhhihojkl

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ require("./routes/cartRoutes")(app);
 require("./routes/billingRoutes")(app);
 if(process.env.NODE_ENV==="production")
 {
-  app.use(express.static(__dirname+"/client/build"));
-  const path=require("path");
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+  app.use(express.static("/client/build"));
+  // const path=require("path");
+  app.get("/*",(req,res)=>{
+    res.sendFile("./client/build/index.html");
   })
 }
 const port = process.env.PORT || 5000;
